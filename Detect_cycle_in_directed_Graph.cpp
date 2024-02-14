f2b8e942a556fa4899ca3f07cf01022bc99efc5b
Tarun Mali <58786864+tarunmali@users.noreply.github.com>
2024-02-14 23:34:01 +0530

    q

--- Detect_cycle_in_directed_Graph.cpp
+++ Detect_cycle_in_directed_Graph.cpp
@@ -5,84 +5,60 @@ using namespace std;
 #include <cstring>
 #include <climits>
 #include <algorithm>
-#include 
+#include <stack>
 
 ll n;
 stack<ll> st;
+ll sum;
 
-ll sum(ll i,ll j, vector<ll> &ps){
-    ll ans=ps[j];
-    if(i!=0) ans-=ps[i-1];
-    return ans;
-}
 
 
 void f(){
-    for(ll x: v){
-        cout<<x<<" ";
-    } 
-    // ll n=v.size();
-    // vector<ll> ps(n);
-    // ps[0]=v[0];
-    // for(ll i=1;i<n;i++){
-    //     ps[i]=ps[i-1]+v[i];
-    // }
-    // ll min_sum=LONG_MAX;
-    // ll i_ans,j_ans;
-    // for(ll i=0;i<n;i++){
-    //     for(ll j=i+2;j<n;j++){
-    //         if(v[i]==v[j] and sum(i,j-1,ps)<min_sum){
-    //             i_ans=i;
-    //             j_ans=j-1;
-    //         }
-    //     }
-    // }
-    // sort(v.begin()+i_ans,v.begin()+j_ans+1);
-    // for(ll i=i_ans;i<=j_ans;i++){
-    //     cout<<v[i]+1<<" ";
-    // }
-    // cout<<'\n';
+    //incomplete
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<"\n";
 }
 
-bool dfs(ll node, ll par,vector<ll> al[],vector<bool> &vis){
+bool dfs(ll node,vector<ll> al[],vector<bool> &vis, vector<bool> &rsFlag){
     vis[node]=1;
-    v.push_back(node);
-
+    st.push(node);
+    rsFlag[node]=1;
     for(ll nei:al[node]){
         if(!vis[nei]){
-            if(dfs(nei,node,al,vis)){
+            if(dfs(nei,al,vis,rsFlag)){
                 return 1;
             } 
         } 
-        else if(par!=nei){
-            v.push_back(nei);
-            return 1;
+        if(rsFlag[node]){
+            //successfully find the cycle
+            st.push(node);
+            //run the logic to extract the present found cycleinfo
+            f();
         }
     }
-    
-    v.pop_back();
+    st.pop();
+    rsFlag[node]=0;
     return 0;
 }
 
 void solve(){
+    sum=LLONG_MAX;
     ll e;
     cin>>n>>e;
     vector<ll> al[n];
-    vector<bool> vis(n,0);
+    vector<bool> vis(n,0),rsFlag(n,0);
     while(e--){
         ll u,v;
         cin>>u>>v;
         u--,v--;
         al[u].push_back(v);
-        // al[v].push_back(u);
     }
     for(ll i=0;i<n;i++){
-        if(!vis[i]) if(dfs(i,-1,al,vis)){
-            f();
-            return;
-        };
+        if(!vis[i]) dfs(i,al,vis,rsFlag);
     }
-    cout<<""<<'\n';
 }
 
 
